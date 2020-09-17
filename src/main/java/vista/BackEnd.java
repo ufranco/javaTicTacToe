8a380cda1f2c1a29e52b65a4279fe92807c71857
ufranco <ulisesjavierfranco@gmail.com>
2020-09-16 23:09:48 -0300

    backend already working, plus now we have junit testing
    Version: 1.0.0

--- src/main/java/vista/BackEnd.java
+++ src/main/java/vista/BackEnd.java
@@ -1,8 +1,11 @@
 package vista;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import vista.MainView.Player;
 
 
 //Comentario de cosos para ganar
@@ -25,7 +28,7 @@ import java.util.stream.Collectors;
 
 public class BackEnd {
 
-	private final  List<ArrayList<Integer>> winSituations = new ArrayList<ArrayList<Integer>>();
+	private List<List<Integer>> winSituations = new ArrayList<List<Integer>>();
 	
 //	(1,2,3), ingeniarse forma de meter esto en el array de arriba sin hardcodearla duro
 //	(4,5,6),
@@ -40,26 +43,45 @@ public class BackEnd {
 //	(8,4,3),
 //	(8,6,1),
 	
-	private List<Character>  plays = new ArrayList<Character>(9);
+	private Player[]  plays = new Player[9];
 	
-	BackEnd(){
-		//initialize winSituations
+	public BackEnd(){
+		
+		Integer[][] harcodeadisimo = new Integer[][] {
+			new Integer[]{1,2,3},
+			new Integer[]{4,5,6},
+			new Integer[]{7,8,9},
+			new Integer[]{1,4,7},
+			new Integer[]{2,5,8},
+			new Integer[]{3,6,9},
+			new Integer[]{1,5,9},
+			new Integer[]{3,5,7},
+			new Integer[]{2,4,9},
+			new Integer[]{2,6,7},
+			new Integer[]{8,4,3},
+			new Integer[]{8,6,1}
+		};		
+			
+		this.winSituations = Arrays.asList(harcodeadisimo).stream()
+				.map(array -> Arrays.asList(array))
+                .collect(Collectors.toList());
 	}
 	
 	
-	public boolean processPlay(Character playerMove, Integer button) {
-		plays.add(button, playerMove);
+	public boolean processPlay(Player playerMove, Integer button) {
+		plays[button] = playerMove;
 		return hayGanador(playerMove);
 	}
 	
 	
-	public boolean hayGanador(Character playerMove) {
+	public boolean hayGanador(Player playerMove) {
 		
-		List<ArrayList<Integer>> possibleWins = this.getWinSituations();
+		List<List<Integer>> possibleWins = this.getWinSituations();
 		Integer index = 0;
 		
-		while(!possibleWins.isEmpty() &&  index < plays.size()) {
-			Character play = plays.get(index); 
+		while(!possibleWins.isEmpty() &&  index < plays.length) {
+			Player play = plays[index]; 
+			
 			++index;
 			
 			if(!playerMove.equals(play)) {
@@ -67,22 +89,25 @@ public class BackEnd {
 			}
 		}
 		
-		return !possibleWins.isEmpty();
+		if(!possibleWins.isEmpty()) {
+			return true;
+		}
 		
+		return false;
 	}
 	
-	public List<ArrayList<Integer>> filterWinConditions(List<ArrayList<Integer>> possibleWins, Integer index){
+	public List<List<Integer>> filterWinConditions(List<List<Integer>> possibleWins, Integer index){
 		return  possibleWins.stream()
 				.filter(winCondition -> !winCondition.contains(index))
 				.collect(Collectors.toList());
 	}
-
-
-	public  List<Character> getPlays() {
-		return this.plays;
+	
+	
+	public void reset(){
+		this.plays = new Player[9];
 	}
 
-	public List<ArrayList<Integer>> getWinSituations() {
+	public List<List<Integer>> getWinSituations() {
 		return this.winSituations;
 	}
 }
--- src/main/java/vista/MainView.java
+++ src/main/java/vista/MainView.java
@@ -13,11 +13,22 @@ import javax.swing.SwingConstants;
 
 public class MainView {
 	
-	enum Player {
+	public enum Player {
 		X,
-		O
+		O,
 	}
-
+	
+	enum GameState{
+		IN_COURSE,
+		WIN,
+		DRAW
+	}
+	
+	
+	public Player playerThisTurn = Player.X;
+	private JLabel playingLabel;
+	
+	private BackEnd backEnd = new BackEnd();
 	private JFrame frame;
 
 
@@ -42,7 +53,6 @@ public class MainView {
 	}
 
 	private void initialize() {
-		
 		frame = new JFrame();
 		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("src/img/favicon.png"));
 		frame.setTitle("totally not a virus.exe");
@@ -56,9 +66,13 @@ public class MainView {
 			
 			public void actionPerformed(ActionEvent e) {
 				
-				if(btn1.getText().isEmpty()) {
-					btn1.setText("O");
+				if(!btn1.getText().isEmpty()) {
+					return;
 				}
+				
+				btn1.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 0);
+				changeTurn(playerThisTurn);
 			}
 		});
 		
@@ -67,49 +81,161 @@ public class MainView {
 		
 		JButton btn2 = new JButton("");
 		btn2.setFont(new Font("Arial", Font.BOLD, 24));
+		
+		btn2.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn2.getText().isEmpty()) {
+					return;
+				}
+				
+				btn2.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 1);
+				changeTurn(playerThisTurn);
+				
+			}
+		});
 		btn2.setBounds(365, 170, 64, 64);
 		frame.getContentPane().add(btn2);
 		
 		JButton btn3 = new JButton("");
 		btn3.setFont(new Font("Arial", Font.BOLD, 24));
+		btn3.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn3.getText().isEmpty()) {
+					return;
+				}
+				
+				btn3.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 2);
+				changeTurn(playerThisTurn);
+			}
+		});
 		btn3.setBounds(439, 170, 64, 64);
 		frame.getContentPane().add(btn3);
 		
 		JButton btn4 = new JButton("");
 		btn4.setFont(new Font("Arial", Font.BOLD, 24));
+		
+		btn4.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn4.getText().isEmpty()) {
+					return;
+				}
+				
+				btn4.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 3);
+				changeTurn(playerThisTurn);
+			}
+		});
 		btn4.setBounds(291, 245, 64, 64);
 		frame.getContentPane().add(btn4);
 		
 		JButton btn5 = new JButton("");
 		btn5.setFont(new Font("Arial", Font.BOLD, 24));
+		btn5.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn5.getText().isEmpty()) {
+					return;
+				}
+				
+				btn5.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 4);
+				changeTurn(playerThisTurn);
+			}
+		});
 		btn5.setBounds(365, 245, 64, 64);
 		frame.getContentPane().add(btn5);
 		
 		JButton btn6 = new JButton("");
 		btn6.setFont(new Font("Arial", Font.BOLD, 24));
+		btn6.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn6.getText().isEmpty()) {
+					return;
+				}
+				
+				btn6.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 5);
+				changeTurn(playerThisTurn);
+			}
+		});
 		btn6.setBounds(439, 245, 64, 64);
 		frame.getContentPane().add(btn6);
 		
 		JButton btn7 = new JButton("");
 		btn7.setFont(new Font("Arial", Font.BOLD, 24));
-		btn7.setBounds(439, 320, 64, 64);
+		btn7.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn7.getText().isEmpty()) {
+					return;
+				}
+				
+				btn7.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 6);
+				changeTurn(playerThisTurn);
+			}
+		});
+		btn7.setBounds(291, 320, 64, 64);
 		frame.getContentPane().add(btn7);
+		
+		JButton btn8 = new JButton("");
+		btn8.setFont(new Font("Arial", Font.BOLD, 24));
+		btn8.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn8.getText().isEmpty()) {
+					return;
+				}
+				
+				btn8.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 7);
+				changeTurn(playerThisTurn);
+			}
+		});
+		btn8.setBounds(365, 320, 64, 64);
+		frame.getContentPane().add(btn8);
 		
 		JButton btn9 = new JButton("");
 		btn9.setFont(new Font("Arial", Font.BOLD, 24));
-		btn9.setBounds(291, 320, 64, 64);
+		btn9.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!btn9.getText().isEmpty()) {
+					return;
+				}
+				
+				btn9.setText(playerThisTurn.toString());
+				backEnd.processPlay(playerThisTurn, 8);
+				changeTurn(playerThisTurn);
+			}
+		});
+		btn9.setBounds(439, 320, 64, 64);
 		frame.getContentPane().add(btn9);
 		
-		JButton btn10 = new JButton("");
-		btn10.setFont(new Font("Arial", Font.BOLD, 24));
-		btn10.setBounds(365, 320, 64, 64);
-		frame.getContentPane().add(btn10);
-		
-		JLabel lblNewLabel_1 = new JLabel("Playing...");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 24));
-		lblNewLabel_1.setBounds(298, 45, 205, 71);
-		frame.getContentPane().add(lblNewLabel_1);
+		playingLabel = new JLabel("Playing: " + playerThisTurn);
+		playingLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		playingLabel.setFont(new Font("Arial", Font.BOLD, 24));
+		playingLabel.setBounds(298, 45, 205, 71);
+		frame.getContentPane().add(playingLabel);
+	}
+	
+	public void changeTurn(Player lastMove) {
+		playerThisTurn = lastMove == Player.X ? Player.O : Player.X;
+		playingLabel.setText("Playing: "+ playerThisTurn);
 	}
 
 }
