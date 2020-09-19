73a633934ea870c4edf0f5bbda8693b720ef8044
ufranco <ulisesjavierfranco@gmail.com>
2020-09-19 00:34:57 -0300

    lectura de wincases implementada

--- src/main/java/vista/BackEnd.java
+++ src/main/java/vista/BackEnd.java
@@ -9,49 +9,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import vista.MainView.Player;
 
 public class BackEnd {
 
-	private List<List<Integer>> winSituations = new ArrayList<List<Integer>>();
-	private InputStream cosoWin;
-
+	private List<List<Integer>> winCases = new ArrayList<List<Integer>>();
 	
 	private Player[]  plays = new Player[9];
 	
 	public BackEnd(){
-		try {
-		String pepe = Files.lines(Paths.get("res/winCases.txt"), StandardCharsets.UTF_8).map(c->c.toString()).collect(Collectors.joining());
-		 System.out.println(
-				 pepe.split(";")[0]
-				 .split(",")[2]);
-		 
-		 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		};
-		Integer[][] harcodeadisimo = new Integer[][] {
-			new Integer[]{1,2,3},
-			new Integer[]{4,5,6},
-			new Integer[]{7,8,9},
-			new Integer[]{1,4,7},
-			new Integer[]{2,5,8},
-			new Integer[]{3,6,9},
-			new Integer[]{1,5,9},
-			new Integer[]{3,5,7},
-			new Integer[]{2,4,9},
-			new Integer[]{2,6,7},
-			new Integer[]{8,4,3},
-			new Integer[]{8,6,1}
-		};		
 			
-		this.winSituations = Arrays.asList(harcodeadisimo).stream()
-				.map(array -> Arrays.asList(array))
-                .collect(Collectors.toList());
+		String unformattedWinCases = "";
+		try {
+			
+			unformattedWinCases = Files.lines(Paths.get("res/winCases.txt"), StandardCharsets.UTF_8)
+										.collect(Collectors.joining());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		//Listo el pollo 
+		this.winCases = Arrays.stream(unformattedWinCases.split(";"))
+								.map(winSituation -> Arrays.stream(winSituation.split(","))
+										.map(Integer::parseInt)
+										.collect(Collectors.toList())
+								)
+								.collect(Collectors.toList());
 	}
 	
 	
@@ -63,7 +47,7 @@ public class BackEnd {
 	
 	public boolean hayGanador(Player playerMove) {
 		
-		List<List<Integer>> possibleWins = this.getWinSituations();
+		List<List<Integer>> possibleWins = getWinCases();
 		Integer index = 0;
 		
 		while(!possibleWins.isEmpty() &&  index < plays.length) {
@@ -94,8 +78,8 @@ public class BackEnd {
 		this.plays = new Player[9];
 	}
 
-	public List<List<Integer>> getWinSituations() {
-		return this.winSituations;
+	public List<List<Integer>> getWinCases() {
+		return this.winCases;
 	}
 	
 }
